e8ed0a13a232373d36d7db7578e5879728390f74
Anshul Karande <anshulkarande20@gmail.com>
2024-09-26 17:25:11 -0400

    final commit, completed assignment three(so sorry for submitting it late, i started late and was stuck)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,14 @@
 import { Component } from '@angular/core';
 import { Coffee } from './interfaces/coffee';
 import { NgFor, NgIf } from '@angular/common';
+import { CoffeeMenuItemsComponent } from './coffee-menu-items/coffee-menu-items.component';
+import { CoffeeShopLocationComponent } from './coffee-shop-location/coffee-shop-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, CoffeeMenuItemsComponent, CoffeeShopLocationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  coffees: Coffee[] = [
-    {
-      name: 'Espresso',
-      origin: 'Ethiopia',
-      roast: 'Dark',
-      price: 2.5,
-      inStock: true,
-    },
-    {
-      name: 'Filter',
-      origin: 'Guatemala',
-      roast: 'Medium',
-      price: 2.0,
-      inStock: false,
-    },
-    {
-      name: 'Americano',
-      origin: 'Brazil',
-      roast: 'Medium',
-      price: 1.5,
-      inStock: true,
-    },
-    {
-      name: 'Latte',
-      origin: 'Colombia',
-      roast: 'Medium',
-      price: 4.0,
-      inStock: true,
-    },
-    {
-      name: 'Cappuccino',
-      origin: 'Costa Rica',
-      roast: 'Medium',
-      price: 3.5,
-      inStock: false,
-    },
-    {
-      name: 'Mocha',
-      origin: 'Sumatra',
-      roast: 'Dark',
-      price: 5.0,
-      inStock: true,
-    },
-  ];
-}
+export class AppComponent {}
--- src/app/coffee-menu-items/coffee-menu-items.component.ts
+++ src/app/coffee-menu-items/coffee-menu-items.component.ts
@@ -1,9 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-coffee-menu-items',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './coffee-menu-items.component.html',
   styleUrl: './coffee-menu-items.component.css',
 })
@@ -14,35 +15,40 @@ export class CoffeeMenuItemsComponent {
       description:
         'Strong coffee brewed by forcing steam through finely-ground coffee beans.',
       price: 2.5,
+      image: 'assets/images/espresso.jpg',
     },
     {
       name: 'Filter',
       Description:
         'Coffee brewed by filtering hot water through ground coffee beans.',
       price: 2.0,
+      image: 'assets/images/filter.jpg',
     },
     {
       name: 'Americano',
       description:
         'Espresso with hot water added to dilute the strength of the coffee.',
       price: 1.5,
+      image: 'assets/images/americano.jpg',
     },
     {
       name: 'Latte',
       description: 'Espresso with steamed milk and a small amount of foam.',
       price: 4.0,
+      image: 'assets/images/latte.jpg',
     },
     {
       name: 'Cappuccino',
       description: 'Espresso with steamed milk and a larger amount of foam.',
       price: 3.5,
+      image: 'assets/images/cappuccino.jpg',
     },
     {
       name: 'Mocha',
       description:
         'Espresso with steamed milk, chocolate syrup, and whipped cream.',
       price: 5.0,
-      image: '../../../assets/images/mocha.jpg',
+      image: 'assets/images/mocha.jpg',
     },
   ];
 }
