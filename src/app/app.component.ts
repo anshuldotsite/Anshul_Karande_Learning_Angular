bdb58e84a1c183a7de52bb577fa61c50138f512a
Anshul Karande <anshulkarande20@gmail.com>
2024-10-10 20:42:57 -0400

    implemented a feature to retrieve a item with an id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Coffee } from './interfaces/coffee';
 import { NgFor, NgIf } from '@angular/common';
 import { CoffeeMenuItemsComponent } from './coffee-menu-items/coffee-menu-items.component';
 import { CoffeeShopLocationComponent } from './coffee-shop-location/coffee-shop-location.component';
+import { CoffeeMenuService } from './services/coffee-menu.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,15 @@ import { CoffeeShopLocationComponent } from './coffee-shop-location/coffee-shop-
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  featuredItem: Coffee | undefined;
+  constructor(private CoffeeMenuService: CoffeeMenuService) {}
+
+  ngOnInit() {
+    this.CoffeeMenuService.getMenu().subscribe({
+      next: (coffeeMenu: Coffee[]) => {
+        this.featuredItem = coffeeMenu.find((item) => item.id === 1);
+      },
+    });
+  }
+}
--- src/app/coffee-menu-items/coffee-menu-items.component.ts
+++ src/app/coffee-menu-items/coffee-menu-items.component.ts
@@ -1,25 +1,14 @@
-import { NgFor } from '@angular/common';
-import { Component, Output } from '@angular/core';
-import { CoffeeItems } from '../data/coffee-items';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { Coffee } from '../interfaces/coffee';
-import { CoffeeMenuService } from '../services/coffee-menu.service';
 
 @Component({
   selector: 'app-coffee-menu-items',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgIf],
   templateUrl: './coffee-menu-items.component.html',
   styleUrl: './coffee-menu-items.component.css',
 })
 export class CoffeeMenuItemsComponent {
-  CoffeeItems: Coffee[] = [];
-  constructor(private CoffeeMenuService: CoffeeMenuService) {}
-
-  ngOnInit(): void {
-    this.CoffeeMenuService.getMenu().subscribe({
-      next: (coffeeMenu: Coffee[]) => {
-        this.CoffeeItems = coffeeMenu;
-      },
-    });
-  }
+  @Input() item: Coffee | undefined;
 }
