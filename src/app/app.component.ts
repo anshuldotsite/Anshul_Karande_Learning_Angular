4d6428dbc7bd8d59c86ec0bdda1f0d3d600cbfc5
Anshul Karande <anshulkarande20@gmail.com>
2024-10-31 15:11:19 -0400

    restarting assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Coffee } from './interfaces/coffee';
+import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
-import { CoffeeMenuItemsComponent } from './coffee-menu-items/coffee-menu-items.component';
-import { CoffeeShopLocationComponent } from './coffee-shop-location/coffee-shop-location.component';
-import { CoffeeMenuService } from './services/coffee-menu.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf, CoffeeMenuItemsComponent, CoffeeShopLocationComponent],
+  imports: [NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  featuredItem: Coffee | undefined;
-  constructor(private CoffeeMenuService: CoffeeMenuService) {}
-
-  ngOnInit() {
-    this.CoffeeMenuService.getMenu().subscribe({
-      next: (coffeeMenu: Coffee[]) => {
-        this.featuredItem = coffeeMenu.find((item) => item.id === 1);
-      },
-    });
-  }
+export class AppComponent {
+  title = 'Coffee Shop';
 }
--- src/app/interfaces/coffee.ts
+++
@@ -1,8 +0,0 @@
-export interface Coffee {
-  id: number;
-  name: String;
-  description: String;
-  price: number;
-  image: String;
-  inStock?: boolean; //optional
-}
